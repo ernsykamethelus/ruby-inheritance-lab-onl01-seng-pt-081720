ba088bad46f6158aa96e4fc9b140af7b881a9a7f
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-22 06:00:38 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -6,3 +6,7 @@ def initialize(first_name, last_name_number)
     @first_name = first_name
     @last_name = last_name
   end
+  
+  user = user.first_name(name)
+  user = user.last_name(name)
+  user
